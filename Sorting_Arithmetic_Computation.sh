ea7797ba4e5cb1cb58ef6f80f19ac9a6bb713b1b
PalakPartani <partanipalak16@gmail.com>
2020-01-22 18:17:04 +0530

    ADDED:Input Variables

--- Sorting_Arithmetic_Computation.sh
+++ Sorting_Arithmetic_Computation.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "Welcome to Sorting arithmatic computation problem"
+echo "Welcome to Sorting arithmetic computation problem"
+echo "Enter value of a : " a
+echo "Enter value of b : " b
+echo "Enter value of c : " c
