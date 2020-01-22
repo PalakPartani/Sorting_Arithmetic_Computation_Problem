c78fcd3479b643d403a108948d0f266a428eadb0
PalakPartani <partanipalak16@gmail.com>
2020-01-22 18:42:05 +0530

    ADDED:Equation Result2

--- Sorting_Arithmetic_Computation.sh
+++ Sorting_Arithmetic_Computation.sh
@@ -4,4 +4,7 @@ read -p "Enter value of a : " a
 read -p "Enter value of b : " b
 read -p "Enter value of c : " c
 result=$(( $a + $b * $c ))
+result2=$(( $a * $b + $c ))
 echo "Result :" $result
+echo "Result :" $result2
+
