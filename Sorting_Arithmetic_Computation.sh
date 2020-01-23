e4d99c8fc9d135890279772201ca17a55ee55bf8
PalakPartani <partanipalak16@gmail.com>
2020-01-23 11:15:33 +0530

    MODIFIED:Store float result

--- Sorting_Arithmetic_Computation.sh
+++ Sorting_Arithmetic_Computation.sh
@@ -1,13 +1,21 @@
-#!/bin/bash
+#!/bin/bash -x
+declare -A dict_result
+declare -a arr_result
+declare -a revarr
 echo "Welcome to Sorting arithmetic computation problem"
 read -p "Enter value of a : " a
 read -p "Enter value of b : " b
 read -p "Enter value of c : " c
 result=$(( $a + $b * $c ))
 result2=$(( $a * $b + $c ))
-result3=$(( $c + $a / $b ))
+result3=$(( $c + $( echo "scale=2; $(($a / $b))" | bc ) ))
 result4=$(( $a % $b + $c ))
 echo "Result :" $result
 echo "Result :" $result2
 echo "Result :" $result3
 echo "Result :" $result4
+
+
+
+
+
